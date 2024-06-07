a1e35153ba2d17dd094d82eef0d532f50396bab2
Tyler Pritchard <pritchard.tyler@gmail.com>
2024-06-06 18:37:25 -0700

    fix dummy hero

--- src/components/main/main.styles.ts
+++ src/components/main/main.styles.ts
@@ -2,7 +2,6 @@ import CSS from 'csstype';
 import Main from '../../assets/images/desktop/TacomaCrows.png';
 
 export const MainWrapper: CSS.Properties = {
-  // position: 'absolute',
   backgroundImage: `url(${Main})`,
   top: '0',
   width: '100%',
@@ -13,14 +12,15 @@ export const MainWrapper: CSS.Properties = {
 };
 
 export const MainTitle: CSS.Properties = {
-  marginTop: '30%',
-  color: 'white',
-  border: '1px solid white',
+  marginBottom: '23%',
+  marginTop: '17%',
+  paddingTop: '4rem',
+  color: '#C47E37',
   padding: '3%',
   top: '5%',
-  width: '90vw',
-  height: '40vh',
-  // h1 {
-  //   font-size: '2.8rem',
-  // }
+  width: '65vw',
+  height: '30vh',
+  textAlign: 'center',
+  backgroundColor: '#FCE4AD',
+  opacity: '90%'
 };
