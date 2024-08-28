e2751f759c14010b0f4be0b54654c87c08627038
Tyler Pritchard <pritchard.tyler@gmail.com>
2024-08-28 15:51:47 -0700

    update middle footer for all screen sizes

--- src/components/footer/footer-middle/footer-middle.styles.ts
+++ src/components/footer/footer-middle/footer-middle.styles.ts
@@ -4,18 +4,23 @@ export const MiddleFooterWrapper = styled.div`
   display: flex;
   flex-direction: row;
   align-items: center;
-  width: 100%;
-  max-width: 100vw;
-  padding: 1rem;
+  width: 100vw;
   overflow-x: hidden;
   height: auto;
   background-color: black;
-    
+  
+  @media (max-width: 480px) {
+    justify-content: center;
+    padding: 1rem;
+  }
   @media  (min-width: 481px) and (max-width: 768px) {
     justify-content: center;
     width: 90vw;
   }
-  @media (min-width: 769px) and (max-width: 992px) {}
+  @media (min-width: 769px) and (max-width: 992px) {
+    justify-content: center;
+    width: 90vw;
+  }
   @media (min-width: 993px) and (max-width: 1200px) {
     width: 100vw;
     justify-content: left;
@@ -28,8 +33,20 @@ export const MiddleInner = styled.div``;
 export const MiddleLogo = styled.img`
   width: 50vw;
 
+  @media (max-width: 480px) {
+    width: 95vw;
+  }
   @media (min-width: 481px) and (max-width: 768px){
-    width: 60vw;
+    width: 70vw;
+  }
+  @media (min-width: 769px) and (max-width: 992px) {
+    width: 75vw;
+  }
+  @media (min-width: 993px) and (max-width: 1200px) {
+    width: 40vw;
+  }
+  @media (min-width: 1201px) {
+    width: 30vw;
   }
 `;
 
