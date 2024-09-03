01a67fab1c0fd755521ac0218b6254b79628df86
Tyler Pritchard <pritchard.tyler@gmail.com>
2024-09-03 16:13:34 -0700

    update for cors errors

--- src/axiosConfig.ts
+++ src/axiosConfig.ts
@@ -1,10 +1,13 @@
 import axios from 'axios';
 
 const axiosInstance = axios.create({
-  baseURL: process.env.REACT_APP_API_BASE_URL || 'https://unique-comfort.up.railway.app',
-  headers: {
-    'Content-Type': 'application/json',
-  },
+    baseURL: process.env.NODE_ENV === 'production'
+        ? 'https://unique-comfort.up.railway.app/api'
+        : 'http://localhost:5000/api',
+    withCredentials: true,
+    headers: {
+        'Content-Type': 'application/json',
+    },
 });
 
 export default axiosInstance;
