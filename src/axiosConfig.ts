c42a3e7365cdc5d91407f500d450b577f361fb50
Tyler Pritchard <pritchard.tyler@gmail.com>
2024-09-05 14:20:05 -0700

    cors

--- src/axiosConfig.ts
+++ src/axiosConfig.ts
@@ -2,7 +2,7 @@ import axios from 'axios';
 
 const axiosInstance = axios.create({
     baseURL: process.env.NODE_ENV === 'production'
-        ? 'https://unique-comfort.up.railway.app/api'
+        ? 'https://rr-auth-production.up.railway.app/api'
         : 'http://localhost:5000/api',
     withCredentials: true,
     headers: {
