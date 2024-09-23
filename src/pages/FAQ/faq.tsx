b402c0f6a6985c7d7b0ec132a3da71a9f94ac000
Tyler Pritchard <pritchard.tyler@gmail.com>
2024-09-23 01:41:42 -0700

    faq

--- src/pages/FAQ/faq.tsx
+++ src/pages/FAQ/faq.tsx
@@ -16,11 +16,17 @@ const faqData = [
   {
     section: "FAN CLUB",
     items: [
-      { question: "What is a Legacy Member?", answer: "A legacy member is ..." },
-      { question: "I Need to Renew My Membership!", answer: "Renewing membership is ..." },
-      { question: "What Is The Best Way To Contact Metallica?", answer: "Contact them at ..." },
-      { question: "What Is The Metallica Club's Mailing Address?", answer: "The mailing address is ..." },
-      { question: "Why Did The Metallica Club Change From Its Old Format?", answer: "The format changed because ..." },
+      { question: "What is a Legacy Member?", answer: "A legacy member is someone who had a paid MetClub membership between 1994 and 2015 at any time for two or more years OR had an active paid membership on August 24, 2015. If you are a Legacy Member a 'Legacy' badge will be displayed on your User Profile located at metallica.com/me." },
+      { question: "I Need to Renew My Membership!", answer: "As of October 2024, being a member is free! There is no need to pay to join or renew your membership." },
+      { question: "What Is The Best Way To Contact Metallica?", answer: "The band has set up The Metallica Club for this very reason. They want to keep fans connected and this is a way that they are able to communicate with you guys. You can write to the band at the Club's mailing address or email info@metclub.com." },
+      { question: "What Is The Metallica Club's Mailing Address?", answer: `
+        THE METALLICA CLUB
+        369-B THIRD STREET
+        PMB # 194
+        SAN RAFAEL, CA 94901-3573
+        USA` 
+    },
+      { question: "Why Did The Metallica Club Change From Its Old Format?", answer: "Napster was shut down and Lars needed something to do." },
     ],
     moreItems: [
       { question: "Show More", answer: "More information..." },
@@ -29,7 +35,7 @@ const faqData = [
   {
     section: "SITE",
     items: [
-      { question: "Submitting Content", answer: "You can submit content by ..." },
+      { question: "Submitting Content", answer: "Do you remember the setlist from a show that isn't posted on the site?  Do you want to scan your ticket stubs so that we can display them in the Tour Section?  Open up a Customer Support ticket and let us know." },
     ],
   },
 ];
