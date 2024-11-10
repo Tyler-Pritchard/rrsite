1015000cd46f08be0f9214a4cebee07da487a1f7
Tyler Pritchard <pritchard.tyler@gmail.com>
2024-11-09 17:56:17 -0800

    vercel test

--- src/components/header/header-left/header-left.tsx
+++ src/components/header/header-left/header-left.tsx
@@ -1,66 +1,68 @@
-import React, { useState } from 'react';
-import {
-    StyledMenuIcon, 
-    StyledCloseIcon, 
-    Container,
-    OpenButton,
-    Drawer,
-    ModalContainer,
-    ModalMenu,
-    ModalMenuItem,
-    ModalMenuLink,
-    MenuItemText
-  } from './header-left.styles';
+// import React, { useState } from 'react';
+// import {
+//     StyledMenuIcon, 
+//     StyledCloseIcon, 
+//     Container,
+//     OpenButton,
+//     Drawer,
+//     ModalContainer,
+//     ModalMenu,
+//     ModalMenuItem,
+//     ModalMenuLink,
+//     MenuItemText
+//   } from './header-left.styles';
 
-// Define an interface for the props
-interface HeaderLeftProps {}
+// // Define an interface for the props
+// interface HeaderLeftProps {}
 
-// Define an interface for menu items
-interface MenuItem {
-    title: string;
-    link: string;
-}
+// // Define an interface for menu items
+// interface MenuItem {
+//     title: string;
+//     link: string;
+// }
 
-const HeaderLeft: React.FC<HeaderLeftProps> = () => {
+// const HeaderLeft: React.FC<HeaderLeftProps> = () => {
 
-    // const [isModalOpen, setIsModalOpen] = useState(false);
-    // // toggle modal state
-    // const toggleModal = (): void => {
-    // //   setIsModalOpen(!isModalOpen);
-    // console.log("modal toggled!")
-    // };
+//     // const [isModalOpen, setIsModalOpen] = useState(false);
+//     // // toggle modal state
+//     // const toggleModal = (): void => {
+//     // //   setIsModalOpen(!isModalOpen);
+//     // console.log("modal toggled!")
+//     // };
   
-    const menuItems: MenuItem[] = [
-        { title: 'Tour', link: '/tour' },
-        { title: 'News', link: '/news' },
-        { title: 'Band', link: '/band/timeline' },
-        { title: 'Music', link: '/store/music' },
-        { title: 'Media', link: '/media' },
-    ];
+//     const menuItems: MenuItem[] = [
+//         { title: 'Tour', link: '/tour' },
+//         { title: 'News', link: '/news' },
+//         { title: 'Band', link: '/band/timeline' },
+//         { title: 'Music', link: '/store/music' },
+//         { title: 'Media', link: '/media' },
+//     ];
 
-    return (
-        <Container>
-            {/* <OpenButton onClick={toggleModal}>
-                <StyledMenuIcon />
-            </OpenButton>
-            {isModalOpen && (
-                <ModalContainer>
-                    <Drawer>
-                        <OpenButton onClick={toggleModal}><StyledCloseIcon />Close</OpenButton>
-                        <ModalMenu>
-                        {menuItems.map((item, index) => (
-                            <ModalMenuItem key={index} onClick={toggleModal}>
-                                <ModalMenuLink href={item.link}>
-                                    <MenuItemText>{item.title}</MenuItemText>
-                                </ModalMenuLink>
-                            </ModalMenuItem>
-                        ))}
-                        </ModalMenu>
-                    </Drawer>
-                </ModalContainer>
-            )} */}
-        </Container>
-    );
-  };
+//     return (
+//         <Container>
+//             {/* <OpenButton onClick={toggleModal}>
+//                 <StyledMenuIcon />
+//             </OpenButton>
+//             {isModalOpen && (
+//                 <ModalContainer>
+//                     <Drawer>
+//                         <OpenButton onClick={toggleModal}><StyledCloseIcon />Close</OpenButton>
+//                         <ModalMenu>
+//                         {menuItems.map((item, index) => (
+//                             <ModalMenuItem key={index} onClick={toggleModal}>
+//                                 <ModalMenuLink href={item.link}>
+//                                     <MenuItemText>{item.title}</MenuItemText>
+//                                 </ModalMenuLink>
+//                             </ModalMenuItem>
+//                         ))}
+//                         </ModalMenu>
+//                     </Drawer>
+//                 </ModalContainer>
+//             )} */}
+//         </Container>
+//     );
+//   };
   
-  export default HeaderLeft;
+//   export default HeaderLeft;
+const HeaderLeft = () => {}
+export default HeaderLeft;
