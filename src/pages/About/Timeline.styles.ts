1e1ebac8bf772fc6295ddcb2a199abf63cddbb54
Tyler Pritchard <pritchard.tyler@gmail.com>
2024-11-10 23:29:19 -0800

    add styling to timeline

--- src/pages/About/Timeline.styles.ts
+++ src/pages/About/Timeline.styles.ts
@@ -1,14 +1,40 @@
 import styled from 'styled-components';
 
 export const TimelineContainer = styled.div`
-  width: 100%;
-  height: 650px;
+  // width: 100%;
+  // height: 650px;
   display: flex;
   justify-content: center;
   align-items: center;
+  top: 10rem;
   iframe {
     width: 100%;
     height: 100%;
     border: none;
+  }
+  @media (max-width: 480px) {
+    height: 45rem;
+    padding: .5rem;
+    padding-top: 3rem;
+  }
+  @media (min-width: 481px) and (max-width: 768px){
+      height: 55rem;
+      padding: .25rem;
+      padding-top: 10rem;
+  }
+  @media (min-width: 769px) and (max-width: 992px) {
+    height: 70rem;
+    padding: 1.5rem;
+    padding-top: 10rem;
+  }
+  @media (min-width: 993px) and (max-width: 1200px) {
+    height: 80rem;
+    padding: 2rem;
+    padding-top: 16rem;
+  }
+  @media (min-width: 1201px) {
+    height: 75rem;
+    padding: 3.5rem; 
+    padding-top: 25rem;
   }
 `;
--- src/pages/About/Timeline.tsx
+++ src/pages/About/Timeline.tsx
@@ -5,7 +5,7 @@ const Timeline: React.FC = () => {
   return (
     <TimelineContainer>
       <iframe 
-        src='https://cdn.knightlab.com/libs/timeline3/latest/embed/index.html?source=1Rbw8wpCESaQ-Dmmw4jyWc8zrbRekwBK1L2rRgbchDbI&font=Default&lang=en&initial_zoom=2&height=650' 
+        src='https://cdn.knightlab.com/libs/timeline3/latest/embed/index.html?source=1Rbw8wpCESaQ-Dmmw4jyWc8zrbRekwBK1L2rRgbchDbI&font=Default&lang=en&start_at_end=true&initial_zoom=2&height=650'
         width='100%' 
         height='650' 
         title="Rob Rich Band Timeline"
