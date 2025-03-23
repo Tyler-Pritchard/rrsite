87ef57224405e7d6e4507275233ab1dbc7be87bc
Tyler Pritchard <pritchard.tyler@gmail.com>
2025-03-23 15:50:01 -0700

    update svg in footer top and background color of timeline

--- src/pages/About/Timeline.styles.ts
+++ src/pages/About/Timeline.styles.ts
@@ -1,4 +1,5 @@
 import styled from 'styled-components';
+import { colors, GlobalVariables } from '../../globalStyles';
 
 /**
  * TimelineContainer is a responsive container for embedding the Knight Lab timeline.
@@ -9,8 +10,11 @@ export const TimelineContainer = styled.div`
   display: flex;
   justify-content: center;
   align-items: center;
-  position: relative; 
+  position: relative;
   z-index: 0;
+  font-family: ${GlobalVariables.paragraphFont};
+  color: ${colors.textPrimary};
+  background-color: ${colors.backgroundBase};
 
   iframe {
     width: 100%;
@@ -21,7 +25,7 @@ export const TimelineContainer = styled.div`
   /* Mobile: Small screens (up to 480px wide) */
   @media (max-width: 480px) {
     height: 98rem;
-    padding: .25rem;
+    padding: 0.25rem;
     top: 7rem;
   }
 
@@ -31,14 +35,14 @@ export const TimelineContainer = styled.div`
     padding: 1rem;
     top: 9rem;
   }
-    
+
   /* Medium screens: tablets in landscape (769px to 992px) */
   @media (min-width: 769px) and (max-width: 992px) {
     height: 90rem;
     padding: 1.5rem;
     top: 10rem;
   }
-      
+
   /* Large screens: smaller desktops (993px to 1200px) */
   @media (min-width: 993px) and (max-width: 1200px) {
     height: 90rem;
