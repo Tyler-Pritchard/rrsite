a8b13b1b4507e11d558e5b2db3f19011b6a95726
Tyler Pritchard <pritchard.tyler@gmail.com>
2025-04-14 23:08:47 -0700

    update styles on /tour

--- src/pages/Tour/tour.styles.ts
+++ src/pages/Tour/tour.styles.ts
@@ -1,26 +1,50 @@
-// rrsite/src/pages/Tour/tour.styles.ts
-
 import styled from 'styled-components';
 import { colors, GlobalVariables } from '../../globalStyles';
 
 export const TourPageWrapper = styled.div`
-  padding: 8rem 2rem;
   background-color: ${colors.backgroundBase};
   color: ${colors.textPrimary};
-  font-family: ${GlobalVariables.paragraphFont};
   min-height: 100vh;
+  padding: 15rem 2rem 4rem 2rem;
+
+
+  @media (max-width: 768px) {
+    padding: 13rem 1.5rem 3rem 1.5rem;
+  }
+
+  @media (max-width: 480px) {
+    padding: 10rem 1rem 2rem 1rem;
+  }
 `;
 
 export const PageTitle = styled.h1`
+  font-family: ${GlobalVariables.titleFont};
+  font-size: ${GlobalVariables.fontSizeXXBig};
+  color: ${colors.highlight};
   text-align: center;
-  font-size: ${GlobalVariables.fontSizeXBig};
   margin-bottom: 4rem;
-  color: ${colors.textPrimary};
+
+  @media (max-width: 768px) {
+    font-size: ${GlobalVariables.fontSizeXBig};
+  }
+
+  @media (max-width: 480px) {
+    font-size: ${GlobalVariables.fontSizeBig};
+  }
 `;
 
 export const EventsGrid = styled.div`
-  display: flex;
-  flex-wrap: wrap;
-  justify-content: center;
-  gap: 2rem;
+  display: grid;
+  gap: 3rem;
+  grid-template-columns: repeat(3, 1fr);
+  justify-items: center;
+
+  @media (max-width: 992px) {
+    grid-template-columns: repeat(2, 1fr);
+  }
+
+  @media (max-width: 768px) {
+    grid-template-columns: 1fr;
+    gap: 2rem;
+  }
 `;
